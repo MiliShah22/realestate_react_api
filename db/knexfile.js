f4246a8de8b38cb1159357654b11e6ea27e3ad47
unknown <mili.shah22>
2026-07-06 15:57:06 +0530

    db connection issue resolved

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,7 +3,23 @@ import 'dotenv/config';
 /** @type {import('knex').Knex.Config} */
 const config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || 'postgresql://estatiq:LHBVtThxzZBbWvCgbt06aSeVAQt1240Q@dpg-d91qlrbsq97s73dni55g-a.oregon-postgres.render.com/estatiq_dev',
+  // connection: process.env.DATABASE_URL || 'postgresql://estatiq:LHBVtThxzZBbWvCgbt06aSeVAQt1240Q@dpg-d91qlrbsq97s73dni55g-a.oregon-postgres.render.com/estatiq_dev',
+  connection: process.env.DATABASE_URL
+    ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+    : {
+      host: 'dpg-d91qlrbsq97s73dni55g-a.oregon-postgres.render.com',
+      database: 'estatiq_dev',
+      user: 'estatiq',
+      password: 'LHBVtThxzZBbWvCgbt06aSeVAQt1240Q',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   pool: {
     min: Number(process.env.DB_POOL_MIN || 2),
     max: Number(process.env.DB_POOL_MAX || 10),
